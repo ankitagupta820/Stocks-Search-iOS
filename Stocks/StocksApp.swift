07878848b5ec78430eecf3dbf0be2aac79ea643c
Ankita Gupta <ankita.er.gupta@gmail.com>
2020-11-25 22:50:32 -0700

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Ankita Gupta on 25/11/20.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
